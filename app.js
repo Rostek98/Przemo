56f1c67205e32fb2cd8f4deac8acda272ff67322
RosSzu <rostek98@email.ua>
2021-03-08 22:36:55 +0100

    Deleted margins, and changed web color

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+$(window).scroll(function(){
+    var scrolledY = $(window).scrollTop();
+    $('header').css('background-position', 'center ' + ((scrolledY)) + 'px');
+})
 
+$(window).scroll(function(){
+    var scrolledY = $(window).scrollTop();
+    $('header').css('background-position', 'center ' + ((scrolledY)) + 'px');
+})
